19d0942c3045003cc01cdbec9dc6878d971eabb6
Артём Мещерин <72318993+st091150@users.noreply.github.com>
2020-11-27 22:44:25 +0500

    Add files via upload

---
+++ ConsoleApplication13.cpp
@@ -0,0 +1,42 @@
+﻿ #include <iostream>
+int main()
+{
+    int n;
+    do {
+        std::cout << "Input n(n>0) :";
+        std::cin >> n;
+        std::cout << std::endl;
+    } while (n <= 0);
+    
+        for (int i = 1; i <= n; ++i) {
+            for (int k = n; k >= 1; --k) {
+                for (int j = 1; j <= n - i; ++j) {
+                    std::cout << " ";
+                }
+                for (int j = 1; j <= 2 * i - 1; ++j) {
+                    std::cout << "*";
+                }
+                for (int j = n; j < 2 * n - i; ++j) {
+                    std::cout << " ";
+                }
+            }
+            std::cout << std::endl;
+        }
+
+        for (int i = n - 1; i >= 1; --i) {
+            for (int k = n; k >= 1; --k) {
+                for (int j = 1; j <= n - i; ++j) {
+                    std::cout << " ";
+                }
+                for (int j = 1; j <= 2 * i - 1; ++j) {
+                    std::cout << "*";
+                }
+                for (int j = n; j < 2 * n - i; ++j) {
+                    std::cout << " ";
+                }
+            }
+            std::cout << std::endl;
+        }
+}
+
+ 
